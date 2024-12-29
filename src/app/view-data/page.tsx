5bd00aae4d316b9c0b5d3b85d4daadb4a6b81964
krisnaganesha1609 <igkganes@gmail.com>
2024-12-29 16:51:42 +0700

    refactor: now only displaying data by date and by country

--- src/app/view-data/page.tsx
+++ src/app/view-data/page.tsx
@@ -1,5 +1,6 @@
 import { promises as fs } from "fs";
 import MyCharts from "../../components/charts";
+import { format } from "date-fns";
 
 export default async function ViewData() {
   const file = await fs.readFile(
@@ -7,26 +8,38 @@ export default async function ViewData() {
     "utf8"
   );
   const data = JSON.parse(file);
-  const osesLabel = data.oses;
-  const osesData = data.oses_total;
-  const countryLabel = data.countries_name;
-  const countryData = data.countries_total;
+  const osesLabel: string[] = data.oses;
+  const osesData: number[] = data.oses_total;
+  const countryLabel: string[] = data.countries_name;
+  const countryData: number[] = data.countries_total;
+  let dateLabel: string[] = data.downloads_date;
+  const dateData: number[] = data.downloads_date_total;
+
+  dateLabel.forEach((element) => {
+    element = format(new Date(element), "dd/MMM/yyyy");
+  });
 
   return (
     <div className={`flex flex-col align-middle items-center`}>
-      <div className=" mt-24 items-center text-center ">
-        <MyCharts
+      <div className="mt-24 items-center text-center">
+        {/* <MyCharts
           title="Data by OS"
           labels={osesLabel}
           datasets={osesData}
-          maxY={20000}
-        />
+          maxY={15000}
+        /> */}
         <div className="my-10" />
         <MyCharts
           title="Data by Country"
           labels={countryLabel}
           datasets={countryData}
-          maxY={5000}
+          maxY={5400}
+        />
+        <MyCharts
+          title="Data by Date"
+          labels={dateLabel}
+          datasets={dateData}
+          maxY={6000}
         />
         <h1>TOTAL DOWNLOAD: {data.total}</h1>
       </div>
